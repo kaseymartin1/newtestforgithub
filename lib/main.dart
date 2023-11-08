b4908bad99787fd13b8a890569d2eb524ef9d9d1
kmartin <kmartin@newcc.gov>
2023-11-08 14:21:37 -0600

    Updated title to v2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test Branch',
+      title: 'Test Branch v2',
       theme: ThemeData(
         // This is the theme of your application.
         //
